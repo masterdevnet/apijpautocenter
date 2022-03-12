dc9d0eed9f341d539fe067e8aba90d458acb5670
masterdevnet <masterdev.net@gmail.com>
2022-03-12 17:28:48 -0300

    Modificado arquivos conexao.php e index.php

--- conexao.php
+++ conexao.php
@@ -4,9 +4,14 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "jpautocenter";
+$port = "3306";
+
+// Cabeçalhos
+header("Acces-Control-Allow-Origin: ");
+header("Content-Type: application/json; charset=UTF-8");
 
 // Conexão com a porta
-//$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
 
 // Conexão se a porta
-$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+//$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
--- index.php
+++ index.php
@@ -9,6 +9,21 @@ $result_produtos->execute();
 
 if (($result_produtos) AND ($result_produtos->rowCount() != 0)) {
 	while ($result_produto = $result_produtos->fetch(PDO::FETCH_ASSOC)) {
-		var_dump($result_produto);
+		extract($result_produto);
+
+		$lista_produtos["records"][$id] = [
+			'id' => $id,
+			'nomeProd' => $nomeProd,
+			'descricaoProd' => $descricaoProd,
+			'precoProd' => $precoProd,
+			'dataCompraProd' => $dataCompraProd,
+			'estoqueProd' => $estoqueProd
+		];
 	}
+
+	// Resposta com status 200
+	http_response_code(200);
+
+	// Retornar os produtos em formato Json
+	echo json_encode($lista_produtos);
 }
